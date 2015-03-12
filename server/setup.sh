98371f4b168e9a80bc98a498b5ede727e4cada13
Nick Landolfi <nclandolfi@gmail.com>
2015-03-11 23:13:44 -0700

    update setup.sh

--- server/setup.sh
+++ server/setup.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
-# Install git, say yes to every prompt
-# apt-get install git -y
-apt-get -y update
-apt-get -y install htop
-
-# Install GVM requirements
-apt-get install curl git mercurial make binutils bison gcc build-essential -y
-# apt-get install mercurial -y
-# apt-get install binutils -y
-# apt-get install bison -y
-# apt-get install gcc -y
-# apt-get install make -y
-
+# Install requirements (GVM et al)
+apt-get update install htop curl git mercurial make binutils bison gcc build-essential -y
 
 # Install GVM
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 source /root/.gvm/scripts/gvm
-gvm install go1.3
-gvm use go1.3 --default
+
+# Install go 1.4
+gvm install go1.4
+gvm use go1.4 --default
 export GOPATH=~/
+
+# Get server
 go get github.com/elos/server
 
-
+# Install mongo
 apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee -a /etc/apt/sources.list.d/10gen.list
-apt-get -y update
 apt-get -y install mongodb-org-server=2.6.1
 mkdir -p /data/db
