08ca94b2b7d192707f8d43779c3d1c0eb9d5217b
Nick Landolfi <nclandolfi@gmail.com>
2015-04-15 22:28:54 -0700

    add final apt-get update

--- server/setup.sh
+++ server/setup.sh
@@ -21,3 +21,5 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee -a /etc/apt/sources.list.d/10gen.list
 apt-get -y install mongodb-org-server=2.6.1
 mkdir -p /data/db
+
+apt-get update
